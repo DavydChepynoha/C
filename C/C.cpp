9f03780d043bfcffe25253a56b6dd56c6132b386
user <user@A202_08>
2025-09-26 18:21:37 +0300

    Добавьте файлы проекта.

---
+++ C/C.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+	int num;
+	std::cout << "Введіть рік: " << std::endl;
+	std::cin >> num;
+
+	if (num % 4 == 0 && num % 100 == 0 && ((num % 100) / 400 == 0)) {
+		std::cout << num << "рік: 366 днів";
+	}
+	else {
+		std::cout << num << "рік: 365 днів";
+	}
+
+	return 0;
+}
